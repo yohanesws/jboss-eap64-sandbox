0be172d936848906043f011c77b8e3f0977f2b85
Yohanes Widi Sono <yohanesws@Yohaness-MacBook-Pro.local>
2017-04-16 10:07:09 +0700

    update cluster example

--- domain_cluster/scripts/eapnode1.sh
+++ domain_cluster/scripts/eapnode1.sh
@@ -1,4 +1,4 @@
 echo "============ Running EAP============ "
-sudo sh -c "echo 'nohup /opt/jboss-eap-6.4/bin/domain.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -c domain.xml --host-config=host.xml -Djboss.node.name=node1 -Djgroups.bind_addr=172.16.2.5 &> /opt/logs/nohup.eap.out&' >> /etc/rc.local"
+sudo sh -c "echo 'nohup /opt/jboss-eap-6.4/bin/domain.sh -b 172.16.2.5 -bmanagement 172.16.2.5 -c domain.xml --host-config=host.xml -Djboss.node.name=node1 -Djgroups.bind_addr=172.16.2.5 &> /opt/logs/nohup.eap.out&' >> /etc/rc.local"
 sudo chmod +x /etc/rc.local
-sudo sh -c 'nohup /opt/jboss-eap-6.4/bin/domain.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -c domain.xml --host-config=host.xml -Djboss.node.name=node1 -Djgroups.bind_addr=172.16.2.5 &> /opt/logs/nohup.eap.out&'
+sudo sh -c 'nohup /opt/jboss-eap-6.4/bin/domain.sh -b 172.16.2.5 -bmanagement 172.16.2.5 -c domain.xml --host-config=host.xml -Djboss.node.name=node1 -Djgroups.bind_addr=172.16.2.5 &> /opt/logs/nohup.eap.out&'
